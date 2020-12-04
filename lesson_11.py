5a25c2103f2a35a130aebeb52595094a16f8e8b9
alwayswannasad <nikitastelmashenko@gmail.com>
2020-12-05 00:51:13 +0200

    add some fixes (WIP)

--- lesson_11.py
+++ lesson_11.py
@@ -1,35 +1,35 @@
 # School - Task - 1
 class Person:
-    def __init__(self, _name):
-        self._name = _name
+    def __init__(self, name: str):
+        self.name = name
         self._time = 24
 
     def get_name(self):
-        return self._name
+        return self.name
 
     def calc_free_time(self, time: int):
         return self._time - int(time)
 
 
 class Student(Person):
-    def __init__(self, _name, _faculty: str):
-        super().__init__(_name)
-        self._faculty = _faculty
+    def __init__(self, name: str, faculty: str):
+        super().__init__(name)
+        self.faculty = faculty
 
     def what_i_must_eat(self):
-        return f"You are a student - eat knowledge's of your faculty - {self._faculty}"
+        return f"You are a student - eat knowledge's of your faculty - {self.faculty}"
 
 
 class Teacher(Person):
-    def __init__(self, _name, _works_days: int = 1, _salary: int = 100):
-        super().__init__(_name)
+    def __init__(self, name, works_days: int = 1, salary: int = 100):
+        super().__init__(name)
 
-        self._salary = _salary
-        self._works_days = _works_days
+        self._salary = salary
+        self._works_days = works_days
 
     def calculate_salary(self):
         salary_by_days = self._works_days * self._salary
-        return f'this is your salary = {salary_by_days} for {self._works_days} day(s)'
+        return salary_by_days
 
 
 student = Student('Phil', 'Math')
@@ -68,36 +68,9 @@ assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]
 
 
 # Task - 3 Store
-# Task 3
-#
-# Product Store
-#
-# Write a class Product that has three attributes:
-#
-# type
-# name
-# price
-# Then create a class ProductStore, which will have some Products and will
-# operate with all products in the store. All methods, in case they can’t perform its action,
-# should raise ValueError with appropriate error information.
-#
-# Tips: Use aggregation/composition concepts while implementing the ProductStore class.
-# You can also implement additional classes to operate on a certain type of product, etc.
-#
-# Also, the ProductStore class must have the following methods:
-#
-# add(product, amount) - adds a specified quantity of a single product with a predefined price premium for your store(30 percent)
-# set_discount(identifier, percent, identifier_type=’name’) - adds a discount for all products specified by input identifiers (type or name).
-# The discount must be specified in percentage
-# sell_product(product_name, amount) - removes a particular amount of products from the store if available,
-# in other case raises an error. It also increments income if the sell_product method succeeds.
-# get_income() - returns amount of many earned by ProductStore instance.
-# get_all_products() - returns information about all available products in the store.
-# get_product_info(product_name) - returns a tuple with product name and amount of items in the store.
-
 
 class Product:
-    def __init__(self, type, name, price):
+    def __init__(self, type: str, name: str, price):
         self.type = type
         self.name = name
         self.price = price
@@ -107,19 +80,19 @@ class Product:
 
 
 class ProductStore:
-    PRODUCTS = []
+    def __init__(self):
+        self.products = []
 
     def percentage(self, percent, whole):
         return (percent * whole) / 100.0
 
     def add(self, product, amount):
         products = {product.type: {
-            product.name: self.percentage(30, product.price),
+            product.name: product.price + self.percentage(30, product.price),
             'amount': amount
         }}
 
-        ProductStore.PRODUCTS.append(products)
-        print(ProductStore.PRODUCTS)
+        self.products.append(products)
 
     def set_discount(self, identifier, percent, identifier_type='name'):
         pass
@@ -139,18 +112,22 @@ class ProductStore:
 
 p = Product('Sport', 'Football T-Shirt', 100)
 p2 = Product('Food', 'Ramen', 1.5)
+p3 = Product('Sport', 'Ball', 200)
 s = ProductStore()
 s.add(p, 10)
 s.add(p2, 300)
+s.add(p3, 12)
 s.sell('Ramen', 10)
 
-
+print(s.products)
 # assert s.get_product_info('Ramen') == ('Ramen', 290)
 
 # Task 4 - Custom exception
 
+
 class CustomException(Exception):
     def __init__(self, msg):
+        super().__init__()
         self.msg = msg
 
     def __str__(self):
