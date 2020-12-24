9b1313f80bef0359912e3b3cc7ab3938e44ad1ab
alwayswannasad <nikitastelmashenko@gmail.com>
2020-12-24 17:10:26 +0200

    change reverse return to f string

--- recursion.py
+++ recursion.py
@@ -63,7 +63,7 @@ def reverse(input_str: str) -> str:
     if len(input_str) == 0:
         return input_str
     else:
-        return reverse(input_str[1:]) + input_str[0]
+        return f'{reverse(input_str[1:])}{input_str[0]}'
 
 
 print(
